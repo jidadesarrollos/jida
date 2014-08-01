e2bb835922c319ea4efa5593618d19900ddcb01d
jrodriguez <jirc48@gmail.com>
2014-08-01 05:57:39 -0430

    merge

--- Helpers/Mensajes.class.php
+++ Helpers/Mensajes.class.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?PHP 
 /**
  * Helper para manejo de mensajes dentro de la aplicación
@@ -14,22 +13,22 @@
  * @version 0.1 12/01/2014
  */
 class Mensajes {
-	
-	
     
-	function __construct(){
-		
-	}//final constructor
-	/**
+    
+    
+    function __construct(){
+        
+    }//final constructor
+    /**
      * Define un arreglo con los valores css para los mensajes
      * 
      * Las clases css a aplicar deben estar definidas en las constantes
      * usadas.
      */
-	static function obtenerEstiloMensaje($clave){
-	    try{
-	        
-	    $estilo=array();
+    static function obtenerEstiloMensaje($clave){
+        try{
+            
+        $estilo=array();
         // if(defined(cssMsjError) and defined(cssMsjAlerta) 
         // and defined(cssMsjSucess) and defined(cssMsjInformacion)){
             $estilo['error']=cssMsjError;
@@ -46,21 +45,10 @@ class Mensajes {
         }catch(Exception $e){
             controlExcepcion($e->getMessage(),$e->getCode());
         }
-	}
-	
-	static function mensajeError($mensaje){
-	   $css = self::obtenerEstiloMensaje('error');
-       $mensaje = "
-                    <DIV class=\"$css\">
-                    <button type=\"button\" class=\"close pull-right\" aria-hidden=\"true\">&times;</button>
-                        $mensaje
-                    </DIV>";
-        return $mensaje;
-        
-	}
+    }
     
-    static function mensajeAlerta($mensaje){
-       $css = self::obtenerEstiloMensaje('alerta');
+    static function mensajeError($mensaje){
+       $css = self::obtenerEstiloMensaje('error');
        $mensaje = "
                     <DIV class=\"$css\">
                     <button type=\"button\" class=\"close pull-right\" aria-hidden=\"true\">&times;</button>
@@ -69,125 +57,6 @@ class Mensajes {
         return $mensaje;
         
     }
-    
-    static function mensajeSuceso($mensaje){
-       $css = self::obtenerEstiloMensaje('suceso');
-       $mensaje = "
-                    <DIV class=\"$css\">
-                        <button type=\"button\" class=\"close pull-right\" aria-hidden=\"true\">&times;</button>
-                        $mensaje
-                    </DIV>";
-        return $mensaje;
-      
-    }
-    
-    static function mensajeInformativo($mensaje){
-       $css = self::obtenerEstiloMensaje('informacion');
-       $mensaje = "
-                    <DIV class=\"$css\">
-                        <button type=\"button\" class=\"close pull-right\" aria-hidden=\"true\">&times;</button>
-                        $mensaje
-                    </DIV>";
-        return $mensaje;
-        
-    }
-    /**
-     * Imprime un mensaje si existe
-     */
-    static function imprimirMensaje($msj=""){
-        if(empty($msj)){
-            if(isset($_SESSION['__msj'])){
-                $msj = $_SESSION['__msj']; 
-            }else{
-                $msj="";
-            }
-        }
-        
-        echo $msj;
-    }
-    /**
-     * Imprime el mensaje guardado en una variable de sesión y luego es destruida la variable
-     * Si no se pasa ningun parametro, la función verificará si existe una variable de sesion 
-     * "__msj".
-     * 
-     * @param string $msj Nombre de la variable de sesión a imprimir.
-     * @method imprimirMsjSesion
-     * 
-     */
-    static function imprimirMsjSesion($msj="__msj"){
-       if(isset($_SESSION[$msj])){ 
-            echo $_SESSION[$msj];
-            Session::destroy($msj);
-        }
-    }
-    
-    static function msjExcepcion($msj,$ruta){
-        $_SESSION['__excepcion'] = $msj;
-        echo $_SESSION['__excepcion'];
-  //      redireccionar($ruta);
-    }
-} // END
-
-=======
-<?PHP 
-/**
- * Helper para manejo de mensajes dentro de la aplicación
- * 
- * Las variables de session para mensajes dentro del jida-framework son:
- * <ul>
- * <li><b>__msjForm</b> Para mensajes en la clase formulario</li>
- * <li><b>__msj </b> Para mensajes en la clase vista y donde se desee.</li>
- * existentes 
- * @package framework
- * @category helper
- * @author  Julio Rodriguez 
- * @version 0.1 12/01/2014
- */
-class Mensajes {
-	
-	
-    
-	function __construct(){
-		
-	}//final constructor
-	/**
-     * Define un arreglo con los valores css para los mensajes
-     * 
-     * Las clases css a aplicar deben estar definidas en las constantes
-     * usadas.
-     */
-	static function obtenerEstiloMensaje($clave){
-	    try{
-	        
-	    $estilo=array();
-        // if(defined(cssMsjError) and defined(cssMsjAlerta) 
-        // and defined(cssMsjSucess) and defined(cssMsjInformacion)){
-            $estilo['error']=cssMsjError;
-            $estilo['alerta']=cssMsjAlerta;
-            $estilo['suceso']=cssMsjSuccess;
-            $estilo['informacion']=cssMsjInformacion;    
-        // }else{
-            // $excepcion = "No se encuentran definidas las constantes css para los mensajes, verifique
-            // el archivo de configuración";
-            // throw new Exception($excepcion, 1);
-        // }
-        
-        return $estilo[$clave];
-        }catch(Exception $e){
-            controlExcepcion($e->getMessage(),$e->getCode());
-        }
-	}
-	
-	static function mensajeError($mensaje){
-	   $css = self::obtenerEstiloMensaje('error');
-       $mensaje = "
-                    <DIV class=\"$css\">
-                    <button type=\"button\" class=\"close pull-right\" aria-hidden=\"true\">&times;</button>
-                        $mensaje
-                    </DIV>";
-        return $mensaje;
-        
-	}
     
     static function mensajeAlerta($mensaje){
        $css = self::obtenerEstiloMensaje('alerta');
@@ -251,5 +120,4 @@ class Mensajes {
     }
 } // END
 
->>>>>>> f8419974b1ea274963edab1038e32d023e18c72d
 ?>
--- Helpers/RedSocial.class.php
+++ Helpers/RedSocial.class.php
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-<?php
-/**
- * Clase Helper para manejo de redes sociales
- * @package Framework
- * @category Helper
- * 
- * @author Julio Rodriguez
- * 
- */
-
-class RedSocial{
-  /**
-   * Crea las etiquetas opengraph necesarias para comprartir una página en facebook
-   * @method fbOpenGraphTags
-   * @access static public
-   */
-    static function fbOpenGraphTags($titulo,$type,$content,$url,$img,$siteName,$descripcion){
-        if(!defined('FACEBOOK_APP_ID')){
-            throw new Exception("No se encuentra definido el id de aplicación de facebook", 1);
-            
-        }
-        $metaTags = 
-        '
-        <meta property="og:title" content="'.$titulo.'"/>
-        <meta property="og:site_name" content="'.$siteName.'"/>
-        <meta property="og:url" content="'.$url.'"/>
-        <meta property="og:description" content="'.$descripcion.'"/>
-        <meta property="og:image" content="'.$img.'"/>
-        <meta property="fb:app_id" content="'.FACEBOOK_APP_ID.'"/>
-        <meta property="og:type" content="'.$type.'"/>
-        
-        ';
-        return $metaTags;
-    }
-}
-=======
 <?php
 /**
  * Clase Helper para manejo de redes sociales
@@ -75,5 +38,4 @@ class RedSocial{
         
     }
 }
->>>>>>> 34cd0f7f25eb4beb9ae94a3d183a38bc9fc66fae
 ?>
