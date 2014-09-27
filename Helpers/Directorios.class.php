2b47d0c9db430556bdbd529d1f46378284a18d5d
jrodriguez <jirc48@gmail.com>
2014-09-27 07:44:30 -0430

    Agregada funcion para listado de archivos en un directorio

--- Helpers/Directorios.class.php
+++ Helpers/Directorios.class.php
@@ -39,6 +39,46 @@ class Directorios {
             }
         }
     }
+        /**
+        * Funcion que recorre y lista todos archivos segun el patron contenido en $expReg 
+        *
+        * @param string $ruta   # directorio a recorrer 
+        * @param string $arr    # arreglo que guarda los archivos recorridos
+        * @param string $expReg # expresion regular para filtrar por el nombre del archivo
+        * @param string $i  # indice
+        
+        * @return $arr array con todos los controladores que coincidan con $expReg
+        */
+        static public function listarDirectoriosRuta($ruta,&$arr,$expReg='',&$i=0){
+        // Abrir un directorio y listarlo recursivamente
+            if (is_dir($ruta)) {
+                if ($directorio = opendir($ruta)) {
+                    while (($file = readdir($directorio)) !== false) {
+                    // Listamos todo lo que hay en el directorio, mostraría tanto archivos como directorios
+                        if(empty($expReg)){
+                        // Guardo todos los archivos recorridos
+                            $arr[$i] = $file;++$i;
+                        }else{
+                        // Guardo los archivos que coincidan con la expresion regular
+                            $esCoincidencia = (preg_match($expReg,$file))?1:0;
+                            if($esCoincidencia){
+                               
+                                $arr[$i] = $file;++$i;
+                            }   
+                        }
+                        if (is_dir($ruta . $file) && $file!="." && $file!=".."){
+                        // Solo si el archivo es un directorio, distinto a "." y ".."
+                        // echo "<br>Directorio: $ruta$file<hr>";
+                        self::listarDirectoriosRuta($ruta . $file . "/",$arr,$expReg,$i);
+                        }
+                    }//fin while
+                    closedir($directorio);
+                }//fin if openRuta
+            }else{
+                throw new Exception("La ruta a listar no es una ruta valida $ruta", 333);
+            }
+            return $arr;
+        }
 } // END
 
 ?>
