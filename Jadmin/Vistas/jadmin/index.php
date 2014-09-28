1c7286471c8ee284e8c79b4892371abccbf9ac37
jrodriguez <jirc48@gmail.com>
2014-09-28 07:27:59 -0430

    agregado layout de una columna para login del modulo administrador jida

--- Jadmin/Vistas/jadmin/index.php
+++ Jadmin/Vistas/jadmin/index.php
@@ -6,16 +6,28 @@
     // echo $_SESSION['msg']."<hr>";
 // }
 ?>
-<h1>Desarrollo de aplicaciones con Jida - Framework</h1>
 
 
 
-<div><?= $dataArray['testBD'] ?></div>
+<?PHP if(isset($dataArray['formLoggin'])){?>
+<!-- <div><?= $dataArray['testBD'] ?></div> -->
+<div class="col-md-6 col-md-offset-3 top-60">
+    <div class="panel panel-default">
+        <section class="panel-heading" role="title">
+            <h1>
+                Jida - Framework
+                <small>Desarrollo de aplicaciones </small>
+            </h1>
+        </section>
+        <section class="panel-body">
+            
+            <?=$dataArray['formLoggin']?>
+        </section>
+    </div>    
+</div>
 <?PHP
-if(isset($dataArray['formLoggin'])){
-    echo $dataArray['formLoggin'];
-}    
-else{
+
+}else{
 ?>
 
 
