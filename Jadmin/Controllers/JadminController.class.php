9a12c216bdc3f814c37e513d09be23562952c3c5
jrodriguez <jirc48@gmail.com>
2015-02-11 15:59:16 -0430

    Eliminado uso de clase Globals deprecada y borrade del framework, actualizado a uso de metodo post del controller en el archivo jadmin y user

--- Jadmin/Controllers/JadminController.class.php
+++ Jadmin/Controllers/JadminController.class.php
@@ -45,8 +45,8 @@ class JadminController extends Controller{
                     $validacion = $form->validarFormulario($_POST);
                     if($validacion===TRUE){
                         $User = new User();
-                        $clave = md5(Globals::obtPost('clave_usuario'));
-                        $checkUser = $User->validarLogin(Globals::obtPost('nombre_usuario'),$clave);
+                        $clave = md5($this->post('clave_usuario'));
+                        $checkUser = $User->validarLogin($this->post('nombre_usuario'),$clave);
                         if($checkUser===FALSE){
                            Formulario::msj('error',"Usuario o clave invalidos");
                         }else{
