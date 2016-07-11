c1f87afc4bb739373d2bb474308224fa2a4201bd
Julio Rodriguez <jirc48@gmail.com>
2016-07-11 01:58:21 -0400

    Mejorado funcionamiento de la clase, corregidos bugs de renderizacion

--- Layout/error.tpl.php
+++ Layout/error.tpl.php
@@ -16,7 +16,7 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
         <?=$this->printHeadTags()?>        
-        <?=$this->printCss()?>
+        <?=$this->imprimirLibrerias('css')?>
     </head>
 
     <body>
