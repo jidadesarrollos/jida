c56041bb3cb6e14b647ebd9ee0165094f90c5a03
Julio Rodriguez <jirc48@gmail.com>
2016-09-15 00:53:45 -0400

    Ajustada clase para que funcione con namespace Jida, agregada funcion imprimir que permite recibir multiples parametrossin importar el tipo

---
+++ Helpers/Debug.php
@@ -0,0 +1,60 @@
+<?PHP
+/**
+ * Clase con funcionalidades generales que permiten al programador hacer tests
+ *
+ *
+ *
+ * @author Julio Rodriguez <jirc48@gmail.com>
+ * @package Framework
+ * @category Helpers
+ * @version 0.1
+ */
+
+namespace Jida;
+class Debug{
+
+    /**
+     * Muestra el contenido de un arreglo envuelto en tag <pre>
+     * @method mostrarArray
+     */
+    static function mostrarArray($ar,$exit=true){
+        echo "\n<pre style=\"background:black;color:#dcdcdc\">\n";
+        print_r($ar);
+        echo "</pre>";
+        if($exit==TRUE){
+            exit;
+        }
+
+    }
+
+	static function imprimir(){
+		$numero = func_num_args();
+		for($i=0;$i<$numero;++$i)
+		{
+			$arg = func_get_arg($i);
+			if(is_array($arg)) self::mostrarArray($arg,0);
+			elseif(is_string($arg)) self::string($arg,0);
+			elseif(is_bool($arg) and $arg){
+				exit;
+
+			}
+		}
+	}
+    /**
+     * Muestra el contenido de una variable String
+     *
+     */
+    static function string($content,$exit=false,$tag="hr"){
+        if(!is_array($content)){
+            echo $content."<$tag/>";
+            if($exit==TRUE){
+                exit;
+            }
+        }elseif(is_array($content) or is_object($content)){
+            self::mostrarArray($content,$exit);
+        }
+
+
+    }
+}
+?>
