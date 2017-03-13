3d36176e1bf0db0dff19e7691eb78d50f75fb54b
Kerby <Kerby>
2017-03-13 12:05:14 -0400

    12:07pm

--- Jadmin/Modulos/Menus/Controllers/MenusController.php
+++ Jadmin/Modulos/Menus/Controllers/MenusController.php
@@ -27,8 +27,7 @@ class MenusController extends \Jida\Jadmin\Controllers\JController {
         $this->layout="jadmin.tpl.php";
         $this->url="/jadmin/menus/";
         parent::__construct();
-        $jctrl = new Modelos\JidaControl();
-        $tablas = $jctrl->obtenerTablasBD();
+
     }
 
     public function index() {
@@ -37,18 +36,17 @@ class MenusController extends \Jida\Jadmin\Controllers\JController {
         $tabla = new Render\jvista('Jida\Modelos\Menus.obtMenus',['titulos'=>['nombre']],'Menus');
 
         $tabla->accionesFila([
-                ['span'=>'glyphicon glyphicon-folder-open','title'=>'Opciones menu','href'=>$this->obtUrl('listarOpciones',['{clave}'])],
-                ['span'=>'glyphicon glyphicon-edit','title'=>'Modificar menu','href'=>$this->obtUrl('',['{clave}'])],
+                ['span'=>'glyphicon glyphicon-folder-open','title'=>'Opciones menu','href'=>'/jadmin/menus/opciones/{clave}'],
+                ['span'=>'glyphicon glyphicon-edit','title'=>'Modificar menu','href'=>$this->obtUrl('gestionMenu',['{clave}'])],
                 ['span'=>'glyphicon glyphicon-trash','title'=>'Eliminar menu','href'=>$this->obtUrl('eliminarMenu',['{clave}']),
                  'data-jvista'=>'confirm','data-msj'=>'<h3>¡Cuidado!</h3>&iquest;Realmente desea eliminar el menu seleccionado?']
             ]);
-
 
         $tabla->addMensajeNoRegistros('No hay Menus Registrados', [
                                                                 'link'  =>$this->obtUrl(''),
                                                                 'txtLink' =>'Crear Menu'
                                                                 ]);
-        $tabla->acciones(['nuevo ' => ['href'=>$this->obtUrl('nuevo')]]);
+        $tabla->acciones(['nuevo ' => ['href'=>$this->obtUrl('gestionMenu')]]);
 
         $this->data(['tablaVista'=>$tabla->obtenerVista()]);
 
@@ -56,38 +54,25 @@ class MenusController extends \Jida\Jadmin\Controllers\JController {
   }
 
 
-
-
-    private function guardarMenu($id='')
+    public function gestionMenu($id='')
     {
-        if ($id!='') {
+        if ($id != '') {
+            $form = new Render\formulario('Menus',$id);
             $classMenu = new Modelos\Menus('menus',$id);
         }else{
-            $classMenu = new Modelos\Menus('menus');
+            $form = new Render\formulario('Menus');  
+            $classMenu = new Modelos\Menus('menus',$id); 
         }
-
-        $classMenu->salvar($this->post());     
-
-    }
-
-
-    /**
-     * Devuelve el formulario para registro y modificación de menues.
-     */
-    function nuevo() {
-         // Helpers\debug::imprimir('holamundo',true);
-        $form = new Render\Formulario('Menus');
 
         $form->boton('principal')->attr('value',"Crear menu");
 
         if ($this->post('btnMenus')) {
             if ($form->validar()) {
-                self::guardarMenu();
+               $classMenu->salvar($this->post()); 
             }
         }
 
         $this->dv->form = $form -> armarFormulario();
-
     }
 
 
@@ -114,119 +99,5 @@ class MenusController extends \Jida\Jadmin\Controllers\JController {
         }
     }//fin funcion
 
-
-
-    public function listarOpciones($id='',$padre=0) {
-
-        $menu = new Modelos\Menus($id);
-        $nombre = $menu->obt();
-
-
-        $tabla = new Render\jvista('Jida\Modelos\OpcionesMenu.obtOpciones',['titulos'=>['Url','Nombre','Orden','estatus']],'Opciones de menu '.$nombre[0]['nombre_menu']);
-
-        $tabla->clausula('filtro',['id_menu'=>$id,'padre'=>$padre]);
-
-
-        $tabla->accionesFila([
-                ['span'=>'glyphicon glyphicon-plus','title'=>'Agregar Opciones','href'=>$this->obtUrl('agregarOpcion',['{clave}',$id])],
-                ['span'=>'glyphicon glyphicon-edit','title'=>'Modificar opcion','href'=>$this->obtUrl('actualizarOpcion',['{clave}',$id])],
-                ['span'=>'glyphicon glyphicon-eye-open','title'=>'ver','href'=>$this->obtUrl('listarOpciones',[$id,'{clave}'])],
-                ['span'=>'glyphicon glyphicon-trash','title'=>'Eliminar opcion','href'=>$this->obtUrl('eliminarOpcion',['{clave}']),
-                 'data-jvista'=>'confirm','data-msj'=>'<h3>¡Cuidado!</h3>&iquest;Realmente desea eliminar el menu seleccionado?']
-            ]);
-
-
-        $tabla->addMensajeNoRegistros('No hay opciones Registradas', [
-                                                                'link'  =>$this->obtUrl(''),
-                                                                'txtLink' =>'Crear Opcion'
-                                                                ]); 
-        $tabla->acciones(['nuevo ' => ['href'=>$this->obtUrl('agregarOpcion',[$id])]]);
-        $tabla->acciones(['volver ' => ['href'=>$this->obtUrl('index')]]);
-
-        $this->data(['tablaOpciones'=>$tabla->obtenerVista()]);
-
-
-  }
-
-    private function guardarOpcion($id='')
-    {
-        if ($id!='') {
-             $opcion = new modelos\opcionesMenu('OpcionMenu',$id,2);
-              helpers\debug::imprimir($opcion,true);
-
-        }else{
-             $opcion = new modelos\opcionesMenu();
-        }
-       
-        $opcion->consulta($id)->salvar($this->post());     
-
-    }
-
-
-  public function agregarOpcion($id='',$padre=''){
-
-        $formulario= new Render\Formulario('RegistroOpcion');
-    
-      
-
-              $formulario->boton('principal')->attr('value',"Crear Opción");
-
-        if ($this->post('btnRegistroOpcion')) {
-            if ($formulario->validar()) {
-
-                self::guardarOpcion();
-                $this->redireccionar('jadmin\menus\index');
-            }
-        }
-
-        $this->data(['tituloForm'=>'Registro De Opciones']);
-        $this->dv->form = $formulario -> armarFormulario();
-  }
-
-
-
-      function eliminarOpcion($id='') {
-
-        if ($this->getEntero($id)) {
-
-            $cMenu = new Modelos\opcionesMenu($id);
-            
-            if(!empty($cMenu->id_opcion_menu)){
-                $cMenu->eliminar($id);
-                // Render\Vista::msj('menus','suceso', 'Menu eliminado');
-
-            }else{
-                // Render\Vista::msj('menus',"error","No se ha eliminado menu");
-            }
-
-            $this->redireccionar('/jadmin/menus/');
-
-
-        }else {
-            throw new Exception("Debe seleccionar un menu", 1);
-        }
-    }//fin funcion
-
-    /**
-     * Funcion controladora de gestion de opciones de un menu
-     * @method procesarOpciones
-     */
-    public function actualizarOpcion($id=''){
-
-        $formulario= new Render\Formulario('RegistroOpcion',$id);
-        $formulario->boton('principal')->attr('value',"Crear Opción");
-
-        if ($this->post('btnRegistroOpcion')) {
-            if ($formulario->validar()) {
-                Helpers\debug::imprimir($_POST,true);
-                $opcion = new modelos\opcionesMenu('OpcionMenu',$id,2);
-                $opcion->salvar($this->post());     
-                $this->redireccionar('\jadmin\menus\index');
-            }
-        }
-        $this->data(['tituloForm'=>'Registro De Opciones']);
-        $this->dv->form = $formulario -> armarFormulario();
-
-    }
 }
 
