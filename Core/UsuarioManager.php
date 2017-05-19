6722407334e9dfb4e5dbef5367ac1e72ac0d8a02
Felix Tovar <felix.godo@gmail.com>
2017-05-19 11:30:08 -0400

    usuario manager parametros

--- Core/UsuarioManager.php
+++ Core/UsuarioManager.php
@@ -22,13 +22,14 @@ trait UsuarioManager{
      * @access protected
      * @param $url del controlador de usuarios que invoca el metodo
      */
-     protected function vistaUser($url=null,$perfiles=false,$titulos=false,$acciones=false){
+     protected function vistaUser($url=null,$perfiles=false,$titulos=false,$acciones=false,$accionesFila=false,$consulta=false,$idVista=false){
 
         if(empty($url)) $url = $this->url;
-		
 		if(!$titulos)	$titulos = ['Usuario','Fecha Creaci&oacute;n','Activo','Ultima Sesi&oacute;n','Estatus'];
-		
-        $jvista = new Render\JVista('\Jida\Modelos\User.obtUsers',['titulos' =>$titulos],'Usuarios');
+		if(!$consulta) 	$consulta='\Jida\Modelos\User.obtUsers';
+		if(!$idVista) 	$idVista = 'Usuarios';
+        
+        $jvista = new Render\JVista($consulta,['titulos' =>$titulos],$idVista);
 
         if($perfiles){
             if(is_array($perfiles))
@@ -37,15 +38,24 @@ trait UsuarioManager{
                 $jvista->clausula('filtro', ['s_usuarios_perfiles.id_perfil' => $perfiles]);
         }
 		
-		if(!$acciones)
-	        $jvista->accionesFila([
+		if($accionesFila)
+	        $jvista->accionesFila($accionesFila);
+		else
+			$jvista->accionesFila([
 	            ['span'=>'glyphicon glyphicon-user','title'=>'Asignar perfiles de acceso','href'=>$this->obtUrl('asociarPerfiles',['{clave}']),'data-jvista'=>'modal'],
 	            ['span'=>'glyphicon glyphicon-edit','title'=>'Modificar usuario','href'=>$this->obtUrl('setUsuario',['{clave}'])],
 	            ['span'=>'glyphicon glyphicon-trash','title'=>'Eliminar Usuario','href'=>$this->obtUrl('eliminarUsuario',['{clave}']),
 	             'data-jvista'=>'confirm','data-msj'=>'<h3>¡Cuidado!</h3>&iquest;Realmente desea eliminar el usuario seleccionado?']
 	        ]);
-
-        $jvista->acciones(['Registrar ' => ['href'=>$this->obtUrl('setUsuario')]]);
+        
+		if(is_array($acciones)):
+	        $jvista->acciones($acciones);
+		else:
+			if($acciones)
+				$jvista->acciones(['Registrar ' => ['href'=>$this->obtUrl('setUsuario')]]);
+		endif;
+				
+				
 
         $jvista->addMensajeNoRegistros('No hay Usuarios Registrados');
 
