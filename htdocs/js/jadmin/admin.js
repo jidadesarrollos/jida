71671b92a0c0404972ccaa388d5cbb98bfc6e915
Julio Rodriguez <jrodriguez@jidadesarrollos.com.ve>
2017-05-21 16:34:32 -0400

    plantilla

--- htdocs/js/jadmin/admin.js
+++ htdocs/js/jadmin/admin.js
@@ -1,173 +1,171 @@
 var Storage = {
-    set:function(key,value){
+	set : function(key, value) {
 
-        if(typeof value == 'object')
-            value = JSON.stringify(value);
-        localStorage[key] = value;
-    },
-    
-    obt :function(key){
-        if(localStorage.getItem(key)!== null){
-            return localStorage.getItem(key);
-        }
-        return false;
-    },
+		if ( typeof value == 'object')
+			value = JSON.stringify(value);
+		localStorage[key] = value;
+	},
 
-    obtJson :function(key){
-        if(this.obt(key)){
-            return JSON.parse(this.obt(key));
-        }
-    },
+	obt : function(key) {
+		if (localStorage.getItem(key) !== null) {
+			return localStorage.getItem(key);
+		}
+		return false;
+	},
 
-    borrar:function(key){
-        localStorage.removeItem(key);
-        return this;
-    }
+	obtJson : function(key) {
+		if (this.obt(key)) {
+			return JSON.parse(this.obt(key));
+		}
+	},
 
+	borrar : function(key) {
+		localStorage.removeItem(key);
+		return this;
+	}
 };
-var menuConfig ={
-	showMenu:true
+var menuConfig = {
+	showMenu : true
 };
-function setLinkMenuClass($linkToggle){
- 	if($linkToggle.hasClass('fa-arrow-right')){
- 		$linkToggle.find('span.fa').removeClass('fa-arrow-right').addClass('fa-arrow-left');
- 	}else{
- 		$linkToggle.find('span.fa').removeClass('fa-arrow-left').addClass('fa-arrow-right');
- 	}
+function setLinkMenuClass($linkToggle) {
+	if ($linkToggle.hasClass('fa-arrow-right')) {
+		$linkToggle.find('span.fa').removeClass('fa-arrow-right').addClass('fa-arrow-left');
+	} else {
+		$linkToggle.find('span.fa').removeClass('fa-arrow-left').addClass('fa-arrow-right');
+	}
 }
-function addMenuTooltip(){
-	
-	$('.menu li a').each(function(k,item){
- 			var $item =$( item );
- 			var $icon =$item.find('.fa');	
-			var $text = $item.find('.inner-text');
-			
-			$text.parent().attr({
-				'data-toggle':'tooltip',
-				
-				'data-placement':'right',
-				'title':$.trim($text.html())
-			});
- 			
- 		});
+
+function addMenuTooltip() {
+
+	$('.menu li a').each(function(k, item) {
+		var $item = $(item);
+		var $icon = $item.find('.fa');
+		var $text = $item.find('.inner-text');
+
+		$text.parent().attr({
+			'data-toggle' : 'tooltip',
+
+			'data-placement' : 'right',
+			'title' : $.trim($text.html())
+		});
+
+	});
 }
-function removeMenuTooltip(){
-	$('.menu li a').each(function(k,item){
- 			var $item =$( item );
- 			var $text = $item.find('.inner-text');		
-			$text.parent().removeAttr('data-toggle');
- 			
+
+function removeMenuTooltip() {
+	$('.menu li a').each(function(k, item) {
+		var $item = $(item);
+		var $text = $item.find('.inner-text');
+		$text.parent().removeAttr('data-toggle');
+
 	});
 	$('.tooltip').remove();
 }
-function toggleMenu(open){
-	if(!open) open = false;
+
+function toggleMenu(open) {
+	if (!open)
+		open = false;
 	var $content = $('#content-wrapper');
 	var menuConfig = Storage.obtJson('menuAdmin');
-		
-	if(!open){
-		if(!$content.hasClass('short-menu')){
+
+	if (!open) {
+		if (!$content.hasClass('short-menu')) {
 			$content.addClass('short-menu');
 			menuConfig.showMenu = false;
 			addMenuTooltip();
 		}
-	}else{
+	} else {
 		$('.li-parent').removeClass('selected').find('ul').removeClass('show');
-		if($content.hasClass('short-menu')){
+		if ($content.hasClass('short-menu')) {
 			$content.removeClass('short-menu');
 			menuConfig.showMenu = true;
 			removeMenuTooltip();
-		}else{	
+		} else {
 			$content.addClass('short-menu');
 			menuConfig.showMenu = false;
 		}
 	}
 
+	if ($content.hasClass('short-menu')) {
+		addMenuTooltip();
+	} else {
+		$('aside li a').each(function(item, key) {
+			var $item = $(item);
+			$item.removeAttr('data-toggle');
+		});
 
- 	if($content.hasClass('short-menu')){
- 		addMenuTooltip();
- 	}else{
- 		$('aside li a').each(function(item,key){
- 			var $item = $(item);
- 			$item.removeAttr('data-toggle');
- 		});
-
- 	}
- 	Storage.set('menuAdmin',menuConfig);
+	}
+	Storage.set('menuAdmin', menuConfig);
 }
 
-function  processUrl(key,value){
-	 key = encodeURI(key); value = encodeURI(value);
+function processUrl(key, value) {
+	key = encodeURI(key);
+	value = encodeURI(value);
 
-    var kvp = document.location.search.substr(1).split('&');
+	var kvp = document.location.search.substr(1).split('&');
 
-    var i=kvp.length; var x; 
-    while(i--){
-        x = kvp[i].split('=');
+	var i = kvp.length;
+	var x;
+	while (i--) {
+		x = kvp[i].split('=');
 
-        if (x[0]==key)
-        {
-            x[1] = value;
-            kvp[i] = x.join('=');
-            break;
-        }
-    }
-    if(i<0) {kvp[kvp.length] = [key,value].join('=');}
-    return kvp.join('&');
+		if (x[0] == key) {
+			x[1] = value;
+			kvp[i] = x.join('=');
+			break;
+		}
+	}
+	if (i < 0) {
+		kvp[kvp.length] = [key, value].join('=');
+	}
+	return kvp.join('&');
 
 }
-(function($){
+
+(function($) {
 	console.log('Jida Administrador');
 	/**
 	 * Variable en sesion de la configuracion menuConfig
-	 * @see menuConfig 
+	 * @see menuConfig
 	 */
 	var dataMenu = Storage.obtJson('menuAdmin');
-	if(dataMenu){
+	if (dataMenu) {
 		toggleMenu(dataMenu.showMenu);
-	}else{
-		Storage.set('menuAdmin',menuConfig);
+	} else {
+		Storage.set('menuAdmin', menuConfig);
 	}
-	
+
 	var $linkToggle = $('.menu-toggle');
-	if($('#content-wrapper').hasClass('short-menu'));
-		addMenuTooltip();
+	if ($('#content-wrapper').hasClass('short-menu'))
+		;
+	addMenuTooltip();
 
-	 $('.li-parent').on('click',function(e){
+	$('.li-parent').on('click', function(e) {
 
-	 	var $this = $( this );
-	 	if($this.find('ul').length>1){
+		var $this = $(this);
+		if ($this.find('ul').length > 1) {
 
-	 		setLinkMenuClass($linkToggle);
-		 	toggleMenu(true);
-	 	}
+			setLinkMenuClass($linkToggle);
+			toggleMenu(true);
+		}
 
+	});
 
-	 });
+	console.log(dataMenu);
 
-	 console.log(dataMenu);	 
-	 // $('.nav-aside').on('click','a',function(e){
-	 	// var $this = $(this);
-	 	// if(!$this.hasClass('menu-toggle')){
-		 	// e.preventDefault();
-		 	// params = processUrl('showmenu',dataMenu.showMenu);
-		 	// console.log(e.target.href+'?'+params);
-		 	// window.location.href = e.target.href+'?'+params;
-	 	// }
-	 // });
-
-	 $linkToggle.on('click',function(){
+	$linkToggle.on('click', function(e) {
+		
+		e.preventDefault();
 		setLinkMenuClass($linkToggle);
-		band = (dataMenu.showMenu)?false:true;
+		band = (dataMenu.showMenu) ? false : true;
 		toggleMenu(band);
 
-	 });
+	});
 
-	 $("body").tooltip({
-	 	selector:'[data-toggle="tooltip"]',
-	 	placement:'right'
-	 });
-	 
+	$("body").tooltip({
+		selector : '[data-toggle="tooltip"]',
+		placement : 'right'
+	});
+
 })(jQuery);
-
 
