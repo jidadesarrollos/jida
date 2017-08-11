56165ca267b1ddac9bc548e341f85103bbeb1cb9
Julio Rodriguez <jrodriguez@jidadesarrollos.com.ve>
2017-08-11 13:09:28 -0400

    code

--- Jadmin/Modulos/Menus/Controllers/MenusController.php
+++ Jadmin/Modulos/Menus/Controllers/MenusController.php
@@ -5,7 +5,7 @@
  * @package Framework
  * @subpackage Jadmin
  * @author  kerby Tabares <krtabares@gmail.com>
- * @version 0.5.1 
+ * @version 0.5.1
  */
 
 namespace Jida\Jadmin\Modulos\menus\Controllers;
@@ -16,88 +16,93 @@ use Jida\Render as Render;
 use Jida\Modelos as Modelos;
 
 
-class MenusController extends \Jida\Jadmin\Controllers\JController {
+class MenusController extends \Jida\Jadmin\Controllers\JController
+{
 
 
-   var $manejoParams=true;
+    var $manejoParams = true;
 
-    function __construct(){
-        $this->layout="jadmin.tpl.php";
-        $this->url="/jadmin/menus/";
+    function __construct()
+    {
+        $this->layout = "jadmin.tpl.php";
+        $this->url = "/jadmin/menus/";
         parent::__construct();
     }
 
-    public function index() {
+    public function index()
+    {
 
         $tabla = new Render\JVista('Jida\Modelos\Menus.obtMenus',
-    								['titulos'=>['nombre']],'Menus'
-								  );
+            ['titulos' => ['nombre']], 'Menus'
+        );
 
         $tabla->accionesFila([
-                ['span'=>'glyphicon glyphicon-folder-open','title'=>'Opciones menu','href'=> URL_BASE . '/jadmin/menus/opciones/{clave}/'],
-                ['span'=>'glyphicon glyphicon-edit','title'=>'Modificar menu','href'=>$this->obtUrl('gestionMenu',['{clave}'])],
-                ['span'=>'glyphicon glyphicon-trash','title'=>'Eliminar menu','href'=>$this->obtUrl('eliminarMenu',['{clave}']),
-                 'data-jvista'=>'confirm','data-msj'=>'<h3>¡Cuidado!</h3>&iquest;Realmente desea eliminar el menu seleccionado?']
-            ]);
+            ['span' => 'glyphicon glyphicon-folder-open', 'title' => 'Opciones menu', 'href' => URL_BASE . '/jadmin/menus/opciones/{clave}/'],
+            ['span' => 'glyphicon glyphicon-edit', 'title' => 'Modificar menu', 'href' => $this->obtUrl('gestionMenu', ['{clave}'])],
+            ['span' => 'glyphicon glyphicon-trash', 'title' => 'Eliminar menu', 'href' => $this->obtUrl('eliminarMenu', ['{clave}']),
+                'data-jvista' => 'confirm', 'data-msj' => '<h3>¡Cuidado!</h3>&iquest;Realmente desea eliminar el menu seleccionado?']
+        ]);
 
         $tabla->addMensajeNoRegistros('No hay Menus Registrados',
-        								['link'  =>$this->obtUrl(''),
-                                         'txtLink' =>'Crear Menu']
-									 );
-        $tabla->acciones(['Nuevo' => ['href'=>$this->obtUrl('gestionMenu')]]);
+            ['link' => $this->obtUrl(''),
+                'txtLink' => 'Crear Menu']
+        );
+        $tabla->acciones(['Nuevo' => ['href' => $this->obtUrl('gestionMenu')]]);
 
-        $this->data(['tablaVista'=>$tabla->obtenerVista()]);
+        $this->data(['tablaVista' => $tabla->render()]);
 
-  }
+    }
 
-    public function gestionMenu($id=''){
-    	
-    	$form = new Render\Formulario('Menus',$id);
+    public function gestionMenu($id = '')
+    {
+
+        $form = new Render\Formulario('Menus', $id);
         $classMenu = new Modelos\Menus($id);
-		
-        $form->boton('principal')->attr('value',"Crear menu");
+
+        $form->boton('principal')->attr('value', "Crear menu");
 
         if ($this->post('btnMenus')) {
-            if ($form->validar()){
-				
-            	$this->post('identificador',Helpers\Cadenas::guionCase($this->post('menu')));
+            if ($form->validar()) {
 
-            	if ($classMenu->salvar($this->post())->ejecutado()):
-               		$tipo = 'suceso';
-					$msj = 'Menu <strong>'.$classMenu->menu.'</strong> creado exitosamente';
-				else:
-					$tipo = 'error';
-					$msj = 'No se pudo crear el menu, por favor, vuelva a intentarlo';
-				endif;
+                $this->post('identificador', Helpers\Cadenas::guionCase($this->post('menu')));
 
-				Render\JVista::msj('menus','suceso', 'Menu <strong>'.$classMenu->menu.'</strong> creado exitosamente',$this->obtUrl('index'));
-			}
+                if ($classMenu->salvar($this->post())->ejecutado()):
+                    $tipo = 'suceso';
+                    $msj = 'Menu <strong>' . $classMenu->menu . '</strong> creado exitosamente';
+                else:
+                    $tipo = 'error';
+                    $msj = 'No se pudo crear el menu, por favor, vuelva a intentarlo';
+                endif;
+
+                Render\JVista::msj('menus', 'suceso', 'Menu <strong>' . $classMenu->menu . '</strong> creado exitosamente', $this->obtUrl('index'));
+            }
         }
-        
+
         $this->dv->usarPlantilla('form');
 
         $this->data(['form' => $form->armarFormulario()]);
     }
 
 
-    function eliminarMenu($id='') {
+    function eliminarMenu($id = '')
+    {
 
         if ($this->entero($id)) {
 
-			$cMenu = new Modelos\Menus($id);
-            
-	        if(!empty($cMenu->id_menu)){
-	        	$cMenu->eliminar($id);
-				// Render\Vista::msj('menus','suceso', 'Menu eliminado');
+            $cMenu = new Modelos\Menus($id);
 
-	        }else{
-	        	// Render\Vista::msj('menus',"error","No se ha eliminado menu");
-	        }
+            if (!empty($cMenu->id_menu)) {
+                $cMenu->eliminar($id);
+                // Render\Vista::msj('menus','suceso', 'Menu eliminado');
+
+            } else {
+                // Render\Vista::msj('menus',"error","No se ha eliminado menu");
+            }
 
             $this->redireccionar('/jadmin/menus/');
 
 
-        }else {
+        } else {
             throw new Exception("Debe seleccionar un menu", 1);
         }
     }//fin funcion
