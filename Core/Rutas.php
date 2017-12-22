538119699b0367009fd0c1453ffcc80fd0cb7dbe
ftovar <felix.godo@gmail.com>
2017-12-22 10:29:47 -0400

    ajustes en armado de ruta de formulario

--- Core/Rutas.php
+++ Core/Rutas.php
@@ -14,87 +14,87 @@ use Jida\Helpers\Sesion;
 
 class Rutas {
 
-    private $_conf;
+	private $_conf;
 
-    private $_solicitud;
+	private $_solicitud;
 
-    private $_rutaAbsoluta;
-    private $_ruta;
+	private $_rutaAbsoluta;
+	private $_ruta;
 
-    function __construct($ruta, $tipo = "") {
+	function __construct($ruta, $tipo = "") {
 
-        $this->_conf = $GLOBALS['JIDA_CONF'];
-        $this->_ruta = $ruta;
-        $this->_solicitud = array_filter(explode("/", $ruta));
+		$this->_conf = $GLOBALS['JIDA_CONF'];
+		$this->_ruta = $ruta;
+		$this->_solicitud = array_filter(explode("/", $ruta));
 
-        switch ($tipo) {
-            case 'formulario':
-                $this->_analizarFormulario();
-                break;
-            default:
-                $this->_analizar();
-                break;
-        }
+		switch ($tipo) {
+			case 'formulario':
+				$this->_analizarFormulario();
+				break;
+			default:
+				$this->_analizar();
+				break;
+		}
 
-    }
+	}
 
-    private function _analizarFormulario() {
+	private function _analizarFormulario() {
 
-        $jida = !!(in_array('jida', $this->_solicitud));
+		$jida = !!(in_array('jida', $this->_solicitud));
 
-        if (count($this->_solicitud) > 1) {
+		if (count($this->_solicitud) > 1) {
 
-            $modulo = array_shift($this->_solicitud);
-            if ($jida) {
-                $form = array_shift($this->_solicitud);
-                $this->_rutaAbsoluta = DIR_FRAMEWORK . DS . 'Formularios' . DS . $form;
-            } else if ($this->_validarModulo($modulo)) {
+			$modulo = array_shift($this->_solicitud);
+			if ($jida) {
+				$form = array_shift($this->_solicitud);
+				$this->_rutaAbsoluta = DIR_FRAMEWORK . DS . 'Formularios' . DS . $form;
+			} else if ($this->_validarModulo($modulo)) {
+				$form = array_shift($this->_solicitud);
+				$this->_rutaAbsoluta = $this->_rutaModulo . DS . 'Formularios' . DS . $form;
+			}
 
-                $form = array_shift($this->_solicitud);
-                $this->_rutaAbsoluta = $this->_rutaModulo . DS . 'Formularios' . DS . $form;
+		} else {
+			$form = array_shift($this->_solicitud);
+			$this->_rutaAbsoluta = DIR_APP . 'Formularios' . DS . $form;
+		}
+	}
 
-            }
-        }
+	private function _limpiar($path, $ds = DS) {
 
+		$array = array_filter(explode($ds, $path));
 
-    }
+		return implode($ds, $array);
+	}
 
-    private function _limpiar($path, $ds = DS) {
+	private function _analizar($solicitud) {
 
-        $array = array_filter(explode($ds, $path));
+		if ($this->_validarModulo($solicitud)) {
+			$this->_rutaAbsoluta = DIR_APP . 'Modulos' . DS . ucwords($solicitud);
+		}
 
-        return implode($ds, $array);
-    }
+	}
 
-    private function _analizar($solicitud) {
+	function absoluta() {
 
-        if ($this->_validarModulo($solicitud)) {
-            $this->_rutaAbsoluta = DIR_APP . 'Modulos' . DS . ucwords($solicitud);
-        }
-
-    }
-
-    function absoluta() {
-
-        return $this->_limpiar($this->_rutaAbsoluta);
-    }
-
-
-    function _validarModulo($modulo, $jida="") {
+		return $this->_limpiar($this->_rutaAbsoluta);
+	}
 
 
-        if (array_key_exists(strtolower($modulo), $this->_conf->modulos)) {
-            $this->_rutaModulo = DIR_APP . 'Modulos' . DS . ucwords($modulo);
+	function _validarModulo($modulo, $jida = "") {
 
-            return true;
-        }
 
-        return false;
-    }
+		if (array_key_exists(strtolower($modulo), $this->_conf->modulos)) {
+			$this->_rutaModulo = DIR_APP . 'Modulos' . DS . ucwords($modulo);
 
-    static function obtener($ruta, $tipo = "") {
+			return TRUE;
+		}
 
-        return new Rutas($ruta, $tipo);
-    }
+		return FALSE;
+	}
+
+	static function obtener($ruta, $tipo = "") {
+
+		return new Rutas($ruta, $tipo);
+	}
 
 }
