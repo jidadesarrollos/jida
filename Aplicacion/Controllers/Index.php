b119676e8c13bf85dd2a9d4fe4463dabe104dcce
Rosmy Rodriguez <rrodriguez@jidadesarrollos.com>
2018-12-07 17:02:03 -0400

    test

--- Aplicacion/Controllers/Index.php
+++ Aplicacion/Controllers/Index.php
@@ -9,9 +9,39 @@ class Index extends App {
 
     function index() {
 
+        $this->layout('principal.tpl.php');
+
+        $this->vista('home');
+
+        $form = new Formulario('jida/Contactanos');
+        $form->boton('principal', 'Enviar solicitud');
+
+        if ($this->post('btnContactanos')) {
+
+            $nombre = $this->post('nombre');
+            $mensaje = $this->post('mensaje');
+
+            if ($form->validar()) {
+                $this->_enviarCorreo([$nombre, $mensaje]);
+            }
+
+            Formulario::msj('error', 'Datos incorrectos');
+
+        }
+
         $this->data([
             'mensaje' => "Somos Jida Desarrollos"
         ]);
+
+    }
+
+    private function _enviarCorreo($post) {
+
+        $correo = new \Jida\Componentes\Correo();
+        $correo->plantilla('contacto')->data([
+            'nombre'  => $post['nombre'],
+            'mensaje' => $post['mensaje']
+        ])->enviar('contacto@jidadesarrollos.com', 'Nuevo mensaje!');
 
     }
 
