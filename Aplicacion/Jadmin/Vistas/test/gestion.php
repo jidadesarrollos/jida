d5c82efbc36e1b14631a313a6cef8beeb7b180dd
jr0driguez <jrodriguez@jidadesarrollos.com>
2019-02-14 23:14:22 -0300

    framework como root

--- Aplicacion/Jadmin/Vistas/test/gestion.php
+++
@@ -1,9 +0,0 @@
-<form enctype="multipart/form-data"
-      action="<?= $this->url ?>"
-      method="POST">
-    <input type="hidden" name="MAX_FILE_SIZE" value="300000"/>
-    Enviar este fichero:
-    <input name="cargaArchivo[]" type="file" multiple/>
-    <hr/>
-    <input type="submit" name="cargaArchivos" CLASS="btn" value="Enviar fichero"/>
-</form>
