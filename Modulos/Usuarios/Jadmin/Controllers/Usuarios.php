aa647a7f757f1f45de9b1f65add8d4a00b15b25b
Alejandro Gomez <alejandrogomez88@gmail.com>
2019-02-25 15:39:39 -0400

    Perfiles

---
+++ Modulos/Usuarios/Jadmin/Controllers/Usuarios.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alejandro
+ * Date: 25/02/19
+ * Time: 03:37 PM
+ */
+
+namespace Jida\Modulos\Usuarios\Jadmin\Controllers;
+
+use Jida\Jadmin\Controllers\JControl;
+
+class Usuarios extends JControl {
+    public function index(){
+
+    }
+}
