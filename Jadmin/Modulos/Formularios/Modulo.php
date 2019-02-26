60a8f8735806284e78c57f605facc49cedfd3521
ever23 <enyerverfranco@gmail.com>
2019-02-26 16:46:21 -0430

    commit -false

--- Jadmin/Modulos/Formularios/Modulo.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Jida\Jadmin\Modulos\Formularios;
-
-class Modulo extends \Jida\Manager\Modulos {
-
-}
--- Jadmin/Modulos/Usuario/Vistas/usuario/login.php
+++
@@ -1,7 +0,0 @@
-<div class="card">
-    <h5 class="card-header text-center">Administrador</h5>
-    <h1><img src="<?= $this->logo ?>" class="login-logo"></h1>
-    <div class="card-body">
-        <?= $this->formulario ?>
-    </div>
-</div>
