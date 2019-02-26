18d25439cfa95082ea0064bc5f6fb4645ced03ce
Alejandro Gomez <alejandrogomez88@gmail.com>
2019-02-26 14:39:15 -0400

    Avances de Perfiles 2

---
+++ Jadmin/Modulos/Usuario/Controllers/Perfiles.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: alejandro
+ * Date: 26/02/19
+ * Time: 02:09 PM
+ */
+
+namespace Jida\Jadmin\Modulos\Usuario\Controllers;
+
+use Jida\Jadmin\Controllers\JControl;
+use Jida\Modulos\Usuarios\Modelos\Perfil;
+
+class Perfiles extends JControl {
+
+    public function index(){
+
+        $listaPerfiles = new Perfil();
+        $listaPerfiles = $listaPerfiles->consulta()->obt();
+        $parametros = ['titulos' => ['Usuario', 'Nombre', 'Apellido', 'Correo', 'Perfiles']];
+        $vista = new JVista($listaPerfiles, $parametros);
+
+        $vista->accionesFila([
+            ['span'  => 'fa fa-edit',
+             'title' => "Editar Perfil",
+             'href'  => '/jadmin/usuario/perfiles/gestion/{clave}'],
+            ['span'        => 'fa fa-trash',
+             'title'       => 'Eliminar Perfil',
+             'href'        => '/jadmin/usuario/perfiles/eliminar/{clave}',
+             'data-jvista' => 'confirm',
+             'data-msj'    => '<h3>¡Cuidado!</h3>&iquest;Realmente desea eliminar el cliente seleccionado?']
+        ]);
+
+        $render = $vista->render();
+
+        $this->data([
+            'vista' => $render
+        ]);
+
+    }
+}
---
+++ Jadmin/Modulos/Usuario/Vistas/perfiles/gestion.php
@@ -0,0 +1,13 @@
+<div class="card">
+    <h4 class="card-header">Usuarios</h4>
+    <div class="card-body">
+        <div id="vista" class="row">
+            <div class="col-12">
+                <?= \Jida\Medios\Sesion::obt('__msj') ?>
+            </div>
+            <div class="col-12">
+                <?= $this->vista ?>
+            </div>
+        </div>
+    </div>
+</div>
