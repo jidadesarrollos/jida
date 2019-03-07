c4c9a97807272e0a6b2d20dfc183b30f655116b8
ever23 <enyerverfranco@gmail.com>
2019-03-07 14:47:47 -0430

    modificacion

--- Validador/README.md
+++ Validador/README.md
@@ -1,1 +1,31 @@
+# Validacion 
 
+Con la clase Jida\Validador\Validador se puede procesar datos validado y sanitizando segun las reglas proporcionadas 
+
+## Metodos: 
+
+### Validador::crear(array $value, array $opciones)
+
+Este es un metodo estatico que procesa los datos y reglas pasados en sus parametros retornando un objeto Validador 
+
+- `array $value`: Valores a Validar y sanitizar
+- `array $opciones`: lista de reglas para cada valor 
+
+Ejemplo:
+```php
+<?php
+$valid = Validador::crear(array_merge($_POST, $_FILES), [
+    'file' => 'archivo|mime_type:image/jpeg,image/jpg,image/png',
+     'email' => 'mail|string:lower'
+]);
+if ($valid->valido()) {
+
+    $name = 'file.' . $valid['file']->getExtension();
+    $valid['file']->copy($name);
+    echo "datos recividos de $valud[email]";
+}
+```
+
+como se puede ver se pueden aplicar varias reglas para cada item las reglas deben estar separadas por el caracter  `| ` y cada regla admite parametros los cuales se pasan colocando `:`  despues de la regla y separando los parametros por `,` las reglas validas son :
+
+- 
--- Validador/reglas/Rstring.php
+++ Validador/reglas/Rstring.php
@@ -18,36 +18,49 @@ class Rstring extends Regla {
     protected $multiple = false;
 
     public function validar($value, array $parametros):bool {
+        
         return is_string($value);
+        
     }
 
     public function processValue($value, array $parametros) {
+        
         foreach ($parametros as $param) {
+            
             switch ($param) {
                 case 'lower':
                     $value = strtolower($value);
                     break;
+                
                 case 'upper':
                     $value = strtoupper($value);
                     break;
+                
                 case 'md5':
                     $value = md5($value);
+                    break;
+                
                 case 'trim':
                     $value = trim($value);
                     break;
+                
                 case 'htmlentities':
                     $value = htmlentities($value);
                     break;
+                
                 case 'htmlencode':
                     $value = filter_var($value, FILTER_SANITIZE_SPECIAL_CHARS);
                     break;
+                
                 case 'urlencode':
                     $value = filter_var($value, FILTER_SANITIZE_ENCODED);
                     break;
             }
+            
         }
 
         return $value;
+        
     }
 
 }
