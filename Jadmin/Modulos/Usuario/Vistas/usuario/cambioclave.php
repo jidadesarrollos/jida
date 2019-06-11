ab6ca8138b5822ae7172a26ce159945712d16594
Rosmy Rodriguez <rrodriguez@jidadesarrollos.com>
2019-06-11 15:29:16 -0400

    modulo cambio de clave

--- Jadmin/Modulos/Usuario/Vistas/usuario/cambioclave.php
+++ Jadmin/Modulos/Usuario/Vistas/usuario/cambioclave.php
@@ -1,8 +1,12 @@
 <div class="card">
     <h4 class="card-header">Cambio de Clave</h4>
     <div class="card-body">
-        <?= \Jida\Medios\Mensajes::imprimirMsjSesion() ?>
-        <?= $this->formulario ?>
-        <div class="clear"></div>
+        <div class="row">
+            <div class="col-md-6">
+                <?= \Jida\Medios\Mensajes::imprimirMsjSesion() ?>
+                <?= $this->formulario ?>
+                <div class="clear"></div>
+            </div>
+        </div>
     </div>
 </div>
