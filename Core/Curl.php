58cb9cdf904235c828014d4caf20f1b0ebf012c7
Rosmy Rodriguez <raynexus@gmail.com>
2019-09-11 19:22:57 -0400

    curl method call

--- Core/Curl.php
+++ Core/Curl.php
@@ -22,11 +22,13 @@ class Curl extends Control {
     private $cookiePath = "/path/";
     /**
      * Url a la que se realiza la llamada por medio del curl
+     *
      * @var url
      */
     var $url;
     /**
      * LLamada curl
+     *
      * @var $curl
      */
     var $curl;
@@ -35,10 +37,11 @@ class Curl extends Control {
 
     /**
      * Funcion constructora para una llamada via curl
+     *
      * @param $url
      * @method __construct
      */
-    function __construct ($url = "", $cookie = null) {
+    function __construct($url = "", $cookie = null) {
 
         $this->id = time();
         $this->url = $url;
@@ -50,9 +53,10 @@ class Curl extends Control {
     /**
      * Inicializa todos los valores del cURL
      * @method init
+     *
      * @param $cookie
      */
-    private function init ($cookie = null) {
+    private function init($cookie = null) {
 
         if ($cookie)
             $this->cookie = $cookie;
@@ -75,7 +79,7 @@ class Curl extends Control {
      * Realiza una petición get
      * @method get
      */
-    function get ($params = [], $url = "", $follow = false) {
+    function get($params = [], $url = "", $follow = false) {
 
         $this->init();
         if (!empty($url))
@@ -106,11 +110,12 @@ class Curl extends Control {
     /**
      * Ejecuta una llamada curl via POST
      * @method llamadaPost
+     *
      * @access public
      * @param array $params Arreglo asociativo de parametos post que se desean enviar en la petición.
      *
      */
-    function post ($params = [], $url = null, $follow = false, $header = false) {
+    function post($params = [], $url = null, $follow = false, $header = false) {
 
         $this->init();
         $elements = [];
@@ -136,9 +141,54 @@ class Curl extends Control {
     }//fin funcón post
 
     /**
+     * Ejecuta una llamada curl
+     * @method call
+     *
+     * @param string $request Metodo de la llamada (get, post, put, delete, etc)
+     * @param string $url Url de la llamada
+     * @param array $params Arreglo de parametos que se desean enviar en la petición.
+     *
+     */
+    function call($method, $url, $params = false) {
+
+        $this->init();
+
+        switch (strtoupper($method)) {
+            case "POST":
+                curl_setopt($this->curl, CURLOPT_POST, 1);
+                if ($params) {
+                    curl_setopt($this->curl, CURLOPT_POSTFIELDS, json_encode($params));
+                }
+                break;
+            case "PUT":
+                curl_setopt($this->curl, CURLOPT_CUSTOMREQUEST, "PUT");
+                if ($params) {
+                    curl_setopt($this->curl, CURLOPT_POSTFIELDS, json_encode($params));
+                }
+                break;
+            default:
+                if ($params) {
+                    $url = sprintf("%s?%s", $url, http_build_query($params));
+                }
+        }
+
+        curl_setopt($this->curl, CURLOPT_URL, $url);
+        curl_setopt($this->curl, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+        curl_setopt($this->curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($this->curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+
+        $result = curl_exec($this->curl);
+        $this->respuesta = $result;
+        $this->cerrarCurl();
+
+        return $this;
+
+    }
+
+    /**
      * Retorna la respuesta obtenida tal y como se recibe
      */
-    function respuesta () {
+    function respuesta() {
 
         return $this->respuesta;
     }
@@ -147,12 +197,12 @@ class Curl extends Control {
      * Retorna la respuesta de la llamada cURL convertida en un objeto
      *
      */
-    function arreglo () {
+    function arreglo() {
 
         return json_decode($this->respuesta, true, 512, JSON_BIGINT_AS_STRING);
     }
 
-    function objeto () {
+    function objeto() {
 
         return json_decode($this->respuesta);
     }
@@ -160,9 +210,10 @@ class Curl extends Control {
     /**
      * Realiza la llamada Curl
      * @method llamadaCurl
+     *
      * @access private
      */
-    private function llamadaCurl () {
+    private function llamadaCurl() {
 
         $response = curl_exec($this->curl);
         $this->cerrarCurl();
@@ -173,14 +224,15 @@ class Curl extends Control {
     /**
      * Cierra la llamada curl
      * @method cerrarCurl
+     *
      * @access private
      */
-    private function cerrarCurl () {
+    private function cerrarCurl() {
 
         curl_close($this->curl);
     }
 
-    private function setParams ($array) {
+    private function setParams($array) {
 
         $params = http_build_query($array);
 
