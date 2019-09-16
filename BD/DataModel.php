e5cfb1ab2427e0306193fc76ee77033ce025f2bb
Rosmy Rodriguez <raynexus@gmail.com>
2019-09-16 17:51:55 -0400

    test

--- BD/DataModel.php
+++ BD/DataModel.php
@@ -2077,6 +2077,7 @@ class DataModel {
         else {
             self::$instancia->query = "CALL " . $sp . ";";
         }
+        Debug::imprimir([self::$instancia->query], true);
         $result = self::$instancia->bd->ejecutarQuery(self::$instancia->query);
         return is_object($result) ? self::$instancia->bd->obtenerDataCompleta($result) : $result;
     }
